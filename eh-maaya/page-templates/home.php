e8e5197b01bfc6995a0d890f47793807090dfb6d
Gautam Singh Chauhan <84838819+gautam120701@users.noreply.github.com>
2023-02-12 16:27:21 +0530

    Update home.php

--- eh-maaya/page-templates/home.php
+++ eh-maaya/page-templates/home.php
@@ -23,9 +23,9 @@
   ?>
     <style>
       .sec1-right-wrap{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/hp_pic1.png'); } 
-      #sec2-card1{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/hp_straight.png'); }
-      #sec2-card2{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/hp_wavy.png'); } 
-      #sec2-card3{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/hp_curly.png'); } 
+      #sec2-card1{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/Straight.png'); }
+      #sec2-card2{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/Wavy.png'); } 
+      #sec2-card3{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/Curly.png'); } 
       .product1-img{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/hp_product1.png'); } 
       .product2-img{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/hp_product2.png'); } 
       .product3-img{ background-image: url('<?php echo THEME_ROOT ?>/assets/images/hp_product3.png'); } 
@@ -47,9 +47,9 @@
               <div class="sec1-inner-wrap">
                 <div class="sec1-left-wrap">
                   <h1 class="title" id="sec1-title">
-                    The Hair care <br />
-                    <a href="#" class="anchor">Ritual</a> <br />
-                    that changed <br />
+                    The Hair care 
+                    <a href="#" class="anchor">Ritual</a> 
+                    that changed <br>
                     Dr. Shweta's life
                   </h1>
                   <button class="btn" id="sec1-btn"><a href="#">Shop</a></button>
@@ -61,11 +61,11 @@
           </div>
           <!-- section2 -->
           <div class="section2">
-            <div class="container">
+            <div class="container" id="sec2-container">
               <div class="inner-wrap">
                 <div class="sec2-heading">
                   <h1 class="title" id="sec2-title">Shop by Hair Type</h1>
-                  <p class="description">
+                  <p class="description" id="sec2-des">
                     Every Hair texture is uniquely gifted. This uniqueness
                     presents its own challenges; we encourage you to embrace this
                     Individuality and leave those challenges to us. We got your
@@ -75,7 +75,7 @@
                 </div>
                 <div class="sec2-cards">
                   <div class="inner-wrap">
-                    <div class="sec2-card img-wrap" id="sec2-card1"> </div>
+                    <div class="sec2-card img-wrap" id="sec2-card1"></div>
                     <div class="sec2-card img-wrap" id="sec2-card2"> </div>
                     <div class="sec2-card img-wrap" id="sec2-card3"></div>
                   </div>
@@ -85,7 +85,7 @@
           </div>
           <!-- section3 -->
           <div class="section3">
-            <div class="container">
+            <div class="">
               <div class="sec3-heading">
                 <h1 class="title" id="sec3-title">Shop by Best Sellers</h1>
                 <p class="description" id="sec3-des">
@@ -102,19 +102,69 @@
                       <div class="product1-img"></div>
                     </div>
                   <div class="product-down">
+                       <div class="line1">
+                                        <h1>Fluidique Conditioner</h1>
+                                        <div class="rating">
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-line"></i>
+                                        </div>
+                                    </div>
+                                    <h2>For Straight Hairs</h2>
+                                    <h3>500 ₹</h3>
+                                    <div class="buttons">
+                                        <button id="btn1"><a href="#">Buy now</a></button>
+                                        <button id="btn2"><a href="#">Add to cart</a></button>
+                                    </div>
                   </div>
                   </div>
                   <div class="product">
                     <div class="product-up">
                       <div class="product2-img"></div>
                     </div>
-
+                    <div class="product-down">
+                      <div class="line1">
+                                        <h1>Fluidique Conditioner</h1>
+                                        <div class="rating">
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-line"></i>
+                                        </div>
+                                    </div>
+                                    <h2>For Straight Hairs</h2>
+                                    <h3>500 ₹</h3>
+                                    <div class="buttons">
+                                        <button id="btn1"><a href="#">Buy now</a></button>
+                                        <button id="btn2"><a href="#">Add to cart</a></button>
+                                    </div>
+                    </div>
                   </div>
                   <div class="product">
                     <div class="product-up">
                       <div class="product3-img"></div>
                     </div>
-
+                    <div class="product-down">
+                       <div class="line1">
+                                        <h1>Fluidique Conditioner</h1>
+                                        <div class="rating">
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-fill"></i>
+                                            <i class="ri-star-line"></i>
+                                        </div>
+                                    </div>
+                                    <h2>For Straight Hairs</h2>
+                                    <h3>500 ₹</h3>
+                                    <div class="buttons">
+                                        <button id="btn1"><a href="#">Buy now</a></button>
+                                        <button id="btn2"><a href="#">Add to cart</a></button>
+                                    </div>
+                  </div>
                   </div>
                   <div class="product mob-only">
                     <div class="product-up">
@@ -127,7 +177,7 @@
           </div>
           <!-- section4 -->
           <div class="section4">
-            <div class="container">
+            <div class="">
               <div class="inner-wrap" id="sec4-inner-wrap">
                 <div class="left-wrap" id="sec4-left-wrap">
                   <h1 class="title" id="sec4-title">Our Beauty Philosophy</h1>
@@ -148,19 +198,15 @@
             </div>
           </div>
           <!-- section5 -->
-          <div class="section5">
-            <div class="container" id="sec5-container">
+
+          <!-- <div class="section5">
+            <div class="container">
               <div class="inner-wrap" id="sec5-innerwrap">
                 <h1 class="title" id="sec5-title">Recommended reads</h1>
                 <div class="sec5-cards">
                   <div class="card">
                     <div class="card-up">
-                      <div class="img-wrap" id="sec5-c1">
-                        <!-- <img
-                          src="images/home page/alexander-krivitskiy-1f7WL1qrrdE-unsplash.png"
-                          alt=""
-                        /> -->
-                      </div>
+                      <div class="img-wrap" id="sec5-c1"></div>
                     </div>
                     <div class="card-down">
                       <h1 class="card-title">Blog title</h1>
@@ -173,10 +219,6 @@
                   <div class="card">
                     <div class="card-up">
                       <div class="img-wrap" id="sec5-c2">
-                        <!-- <img
-                          src="images/home page/ali-pazani-3w14X-Yxffk-unsplash.png"
-                          alt=""
-                        /> -->
                       </div>
                     </div>
                     <div class="card-down">
@@ -189,10 +231,6 @@
                   <div class="card">
                     <div class="card-up">
                       <div class="img-wrap" id="sec5-c3">
-                        <!-- <img
-                          src="images/home page/taisiia-stupak-mWABE4JCUHI-unsplash.png"
-                          alt=""
-                        /> -->
                       </div>
                     </div>
                     <div class="card-down">
@@ -206,59 +244,45 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
+          
           <!-- section6 -->
-          <div class="section6">
-            <div class="container" id="sec6-container">
-              <div class="inner-wrap" id="sec6-inner-wrap">
+          <!-- <div class="section6">
+            <div class="" id="sec6-container">
+              <div class="inner-wrap">
                 <h1 class="title" id="sec6-title">Hear it from the Sisterhood</h1>
-                <div class="sec6-cards">
+                <div class="cards-wrap">
                   <div class="card">
                    <div class="card-up">
-                   <div class="img-wrap" id="sec6-c1">
-                    </div>
+                    <div class="img-wrap" id="sec6-c1"></div>
                    </div>
                   </div>
                   <div class="card">
-                    <div class="img-wrap" id="sec6-c2">
-                      <!-- <img src="images/home page/sec6.png" alt="" /> -->
-                    </div>
+                    <div class="img-wrap" id="sec6-c2"></div>
                   </div>
                   <div class="card">
-                    <div class="img-wrap" id="sec6-c3">
-                      <!-- <img src="images/home page/sec6.png" alt="" /> -->
-                    </div>
+                    <div class="img-wrap" id="sec6-c3"></div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
+          
           <!-- section7 -->
-          <div class="section7">
-            <div class="container" id="sec7-container">
+          <!-- <div class="section7">
+            <div class="" id="sec7-container">
               <div class="inner-wrap" id="sec7-inner-wrap">
                 <div class="sec7-left-wrap">
-                  <h1 class="title" id="sec7-title">
-                    Join the <br>
-                    Sisterhood
-                  </h1>
-                  <p class="para" id="sec7-para">
-                    Get tips, tricks, and a sneak peek into upcoming products!
-                  </p>
-                  <!-- <label> Email </label> -->
-                  <input
-                    class="input"
-                    type="text"
-                    placeholder="Email or phone number"
-                  />
+                  <h1 class="title" id="sec7-title">Join the Sisterhood</h1>
+                  <p class="para" id="sec7-para"> Get tips, tricks, and a sneak peek into upcoming products!</p>
+                  <input class="input" type="text" placeholder="Enter your Email"/>
                   <br />
                   <button class="btn" id="sec4-btn">Submit</button>
                 </div>
-                <div class="sec7-right-wrap">
-                </div>
+                <div class="sec7-right-wrap"></div>
               </div>
             </div>
-          </div>
+          </div> -->
     
           <?php  wp_reset_postdata(); ?>
       <?php
